d7b857eccdfb8d943ebca826402e99e9c01a3140
nharalam32 <nharalam32@gmail.com>
2024-11-19 13:40:39 -0500

    newRoster

---
+++ OldHomeApplication/resources/views/newRoster.blade.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>New Roster</title>
+    </head>
+
+    <body>
+        <h1>New Roster</h1>
+        <h3>Date: </h3>
+        <input type=date>
+        <h3>Supervisor: </h3>
+        <input list="supervisors">
+            <datalist id="supervisors">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <h3>Doctor: </h3>
+        <input list="doctors">
+            <datalist id="doctors">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <h3>Caregiver 1: </h3>
+        <input list="caregiver_1">
+            <datalist id="caregiver_1">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <h3>Caregiver 2: </h3>
+        <input list="caregiver_2">
+            <datalist id="caregiver_2">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <h3>Caregiver 3: </h3>
+        <input list="caregiver_3">
+            <datalist id="caregiver_3">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <h3>Caregiver 4: </h3>
+        <input list="caregiver_4">
+            <datalist id="caregiver_4">
+                <option value="Option 1">
+                <option value="Option 2">
+                <option value="Option 3">
+                <option value="Option 4">
+            </datalist>
+        <br><br>
+        <button>Submit</button><br>
+        <button>Back</button>
+    </body>
+</html>
