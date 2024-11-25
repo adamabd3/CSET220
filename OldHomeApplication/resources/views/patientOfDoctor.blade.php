a0da3e78a53f82ea05dcc29844be9eb2e5088d5b
nharalam32 <nharalam32@gmail.com>
2024-11-25 14:59:48 -0500

    asuafiufsoiuheu

--- OldHomeApplication/resources/views/patientOfDoctor.blade.php
+++ OldHomeApplication/resources/views/patientOfDoctor.blade.php
@@ -7,6 +7,7 @@
         <h1>Patient of Doctor:</h1>
         <h2>{{ $patient->first_name }}</h2>
         <p>{{ $currentDate }}</p>
+        <h3>Prescription:</h3>
         <table>
             <thead>
                 <tr>
@@ -27,5 +28,18 @@
                 @endforeach
             </tbody>
         </table>
+        <h3>New Prescription:</h3>
+        <form action="">
+            <label>Comment:</label>
+            <input><br>
+            <label>Morning Med:</label>
+            <input><br>
+            <label>Afternoon Med:</label>
+            <input><br>
+            <label>Night Med:</label>
+            <input><br>
+            <button>Submit</button>
+            <button>Clear</button>
+        </form>
     </body>
 </html>
