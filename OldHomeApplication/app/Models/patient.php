2e9ac92d67ac879d61e3a8d255770e1145d08acc
gagekta <gc.99hockey@gmail.com>
2024-11-27 11:54:38 -0500

    Update patient.php

--- OldHomeApplication/app/Models/patient.php
+++ OldHomeApplication/app/Models/patient.php
@@ -4,24 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class patient extends Model
+class Patient extends Model
 {
-    protected $table = 'patients';
     protected $fillable = [
-        'patient_id',
-        'first_name',
-        'last_name',
-        'email',
-        'phone',
-        'password',
-        'dob',
-        'family_code',
-        'emergency_contact',
-        'relation_to_contact',
-        'group_number',
-        'admission_date',
-        'approved'
+        "patient_id",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "password",
+        "dob",
+        "family_code",
+        "emergency_contact",
+        "relation_to_contact",
+        "group_number",
+        "admission_date",
+        "approved"
     ];
-    public $timestamps = false;
-    protected $primaryKey = 'patient_id';
+    public $timestamp = false;
+
+    protected $primaryKey = "patient_id";
 }
