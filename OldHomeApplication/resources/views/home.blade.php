8001b4925e846d4416c1cfe923cbc049f54b6c48
gagekta <gc.99hockey@gmail.com>
2024-12-03 12:27:02 -0500

    sfdg

--- OldHomeApplication/resources/views/home.blade.php
+++ OldHomeApplication/resources/views/home.blade.php
@@ -3,5 +3,13 @@
 @section('content')
     <div class="container">
         <h1>home</h1>
+        <form action="{{ route('register') }}" method="GET">
+            @csrf
+            <button type="submit">Pending Accounts</button>
+        </form>
+        <form action="{{ route('login') }}" method="GET">
+            @csrf
+            <button type="submit">Pending Accounts</button>
+        </form>
     </div>
 @endsection
