556b955db41ea85ec06596350e0a604cb9044a6b
adamabd3 <150284767+adamabd3@users.noreply.github.com>
2024-12-03 11:19:32 -0500

    Update Patient.php

--- OldHomeApplication/app/Models/Patient.php
+++ OldHomeApplication/app/Models/Patient.php
@@ -4,24 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Patient extends Model
+class patient extends Model
 {
+    protected $table = 'patients';
     protected $fillable = [
-        "patient_id",
-        "first_name",
-        "last_name",
-        "email",
-        "phone",
-        "password",
-        "dob",
-        "family_code",
-        "emergency_contact",
-        "relation_to_contact",
-        "group_number",
-        "admission_date",
-        "approved"
+        'patient_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'password',
+        'dob',
+        'family_code',
+        'emergency_contact',
+        'relation_to_contact',
+        'group_number',
+        'admission_date',
+        'approved'
     ];
-    public $timestamp = false;
-
-    protected $primaryKey = "patient_id";
+    public $timestamps = false;
+    protected $primaryKey = 'patient_id';
 }
