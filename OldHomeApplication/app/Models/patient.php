323a66e26abc2ec797aa30b885c15095e51548a9
adamabd3 <150284767+adamabd3@users.noreply.github.com>
2024-12-10 12:28:36 -0500

    Update patient.php

--- OldHomeApplication/app/Models/patient.php
+++ OldHomeApplication/app/Models/patient.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Patient extends Model
+class Patient extends Authenticatable
 {   
     protected $primaryKey = 'patient_id';
     protected $table = 'patients';
